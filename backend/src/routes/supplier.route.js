1057557b2863680e35367f92ec11928985de55e0
Abdullah <abdullahiqbal401@gmail.com>
2020-12-19 15:56:25 +0500

    supplier

--- backend/src/routes/supplier.route.js
+++ backend/src/routes/supplier.route.js
@@ -10,7 +10,7 @@ const awaitHandlerFactory = require('../middleware/awaitHandlerFactory.middlewar
 const { createSupplierSchema, updateSupplierSchema} = require('../middleware/validators/supplierValidator.middleware');
 
 
-router.get('/',auth(), awaitHandlerFactory(supplierController.getAllsupplier)); //localhost:3000/api/v1/supplier
+router.get('/',auth(), awaitHandlerFactory(supplierController.getAllSuppliers)); //localhost:3000/api/v1/suppliers
 router.get('/id/:id', auth(),awaitHandlerFactory(supplierController.getsupplierById)); //localhost:3000/api/v1/supplier/id/1
 router.get('/username/:username', auth(),awaitHandlerFactory(supplierController.getSupplierBycode)); //localhost:3000/api/v1/supplier/suppliercode/julia
 router.post('/', auth(),createSupplierSchema, awaitHandlerFactory(supplierController.createSupplier)); //localhost:3000/api/v1/suppliers
