f8e53b9fb9affeecdc717446cfcb7368117a3965
aziz.rehman <aziz.rehman@alfa.de>
2021-01-04 18:00:19 +0100

    booking form styling

--- frontend/src/app/components/booking/BookingDetailsForm.js
+++ frontend/src/app/components/booking/BookingDetailsForm.js
@@ -1,4 +1,4 @@
-import { Box } from "@material-ui/core";
+import { Box, Grid, Typography } from "@material-ui/core";
 import React, { Component } from "react";
 import { BooleanInput, Button, TextInput, Toolbar } from "react-admin";
 class BookingDetailsForm extends Component {
@@ -6,12 +6,12 @@ class BookingDetailsForm extends Component {
     e.preventDefault();
     this.props.nextStep();
   };
-  
-
-
 
   render() {
     const { booking, step, handleChange } = this.props;
+    const small = 6;
+    const xsmall = 12;
+    const medium = 6;
     const styles = {
       button: {
         margin: 15,
@@ -22,42 +22,176 @@ class BookingDetailsForm extends Component {
     if (step == 1) {
       return (
         <Box p="1em">
-          <Box title="Enter User details">
-            <TextInput disabled source="id" />
-            <TextInput source="code" defaultValue={booking.code} />
-            <TextInput source="scode" /*options={{ multiLine: true }}*/ />
-            <TextInput multiline source="title" />
-            <TextInput source="unit" defaultValue={booking.unit}/>
-            <TextInput source="client"defaultValue={booking.client} /> 
-            <TextInput source="book_date"defaultValue={booking.book_date} />
-            <TextInput source="sale_price" defaultValue={booking.sale_price}/>
-            <TextInput source="discount"defaultValue={booking.discount} />
-            <TextInput source="remarks"defaultValue={booking.remarks} />
-            <TextInput source="name" defaultValue={booking.name}/>
-            <TextInput source="father_name"defaultValue={booking.father_name} />
-            <TextInput source="residential_address"defaultValue={booking.residential_address} />
-            <TextInput source="phone_no" defaultValue={booking.phone_no}/>
-            <TextInput source="occupation"defaultValue={booking.occupation} />
-            <TextInput source="nationality"defaultValue={booking.nationality} />
-            <TextInput source="reference_off"defaultValue={booking.reference_off}/>
-            <TextInput source="nominee_name"defaultValue={booking.nominee_name} />
-            <TextInput source="relation"defaultValue={booking.relation} />
-            <TextInput source="cnic" defaultValue={booking.cnic}/>
-            <TextInput source="project"defaultValue={booking.project} />
-            <TextInput source="email"defaultValue={booking.unit} />
-            <BooleanInput source="active" defaultValue={booking.active}/>
+          <Typography variant="h6" gutterBottom>
+            Booking Details
+          </Typography>
+          <div>
+            <Grid container spacing={10}>
+              <Grid
+               
+                spacing={1}
+                id="left-container"
+                item
+                xs={xsmall}
+                md={6}
+              >
+                <Grid container spacing={2}>
+                  <Grid item xs={xsmall} sm={small} md={medium} >
+                    <TextInput disabled source="id" fullWidth />
+                  </Grid>
+                  <Grid item xs={xsmall} sm={small} md={medium}>
+                    <TextInput
+                      source="project"
+                      defaultValue={booking.project}
+                      fullWidth
+                    />
+                  </Grid>
+                  <Grid item xs={xsmall} sm={small} md={medium}>
+                    <TextInput disabled source="code" fullWidth />
+                  </Grid>
+                  <Grid item xs={xsmall} sm={small} md={medium}>
+                    <TextInput
+                      source="scode"
+                      fullWidth /*options={{ multiLine: true }}*/
+                    />
+                  </Grid>
+                  <Grid item xs={xsmall} sm={small} md={medium}>
+                    <TextInput fullWidth multiline source="title" />
+                  </Grid>
+                  <Grid item xs={xsmall} sm={small} md={medium}>
+                    <TextInput
+                      fullWidth
+                      source="unit"
+                      defaultValue={booking.unit}
+                    />
+                  </Grid>
+                  <Grid item xs={xsmall} sm={small} md={medium}>
+                    <TextInput
+                      source="client"
+                      defaultValue={booking.client}
+                      fullWidth
+                    />
+                  </Grid>
+                  <Grid item xs={xsmall} sm={small} md={medium} lg={medium}>
+                    <TextInput
+                      source="book_date"
+                      defaultValue={booking.book_date}
+                      fullWidth
+                    />
+                  </Grid>
+                  <Grid item xs={xsmall} sm={small} md={medium}>
+                    <TextInput
+                      source="sale_price"
+                      defaultValue={booking.sale_price}
+                      fullWidth
+                    />
+                  </Grid>
+                  <Grid item xs={xsmall} sm={small} md={medium}>
+                    <TextInput
+                      source="remarks"
+                      defaultValue={booking.remarks}
+                      fullWidth
+                    />
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid spacing={2} id="right-container" item xs={xsmall} md={6}>
+                <Grid container spacing={1}>
+                  <Grid item xs={xsmall} sm={small} md={medium} lg={medium}>
+                    <TextInput
+                      source="name"
+                      defaultValue={booking.name}
+                      fullWidth
+                    />
+                  </Grid>
+                  <Grid item xs={xsmall} sm={small} md={medium}>
+                    <TextInput
+                      source="father_name"
+                      defaultValue={booking.father_name}
+                      fullWidth
+                    />
+                  </Grid>
+                  <Grid item xs={xsmall} sm={small} md={medium}>
+                    <TextInput
+                      source="cnic"
+                      defaultValue={booking.cnic}
+                      fullWidth
+                    />
+                  </Grid>
+                  <Grid item xs={xsmall} sm={small} md={medium}>
+                    <TextInput
+                      source="phone_no"
+                      defaultValue={booking.phone_no}
+                      fullWidth
+                    />
+                  </Grid>
+                  <Grid item xs={xsmall}>
+                    <TextInput
+                      source="residential_address"
+                      defaultValue={booking.residential_address}
+                      fullWidth
+                    />
+                  </Grid>
+
+                  <Grid item xs={xsmall} sm={small} md={medium}>
+                    <TextInput
+                      source="occupation"
+                      defaultValue={booking.occupation}
+                      fullWidth
+                    />
+                  </Grid>
+                  <Grid item xs={xsmall} sm={small} md={medium}>
+                    <TextInput
+                      source="nationality"
+                      defaultValue={booking.nationality}
+                      fullWidth
+                    />
+                  </Grid>
+                  <Grid item xs={xsmall} sm={small} md={medium}>
+                    <TextInput
+                      source="reference_off"
+                      defaultValue={booking.reference_off}
+                      fullWidth
+                    />
+                  </Grid>
+                  <Grid item xs={xsmall} sm={small} md={medium}>
+                    <TextInput
+                      source="nominee_name"
+                      defaultValue={booking.nominee_name}
+                      fullWidth
+                    />
+                  </Grid>
+                  <Grid item xs={xsmall} sm={small} md={medium}>
+                    <TextInput
+                      source="relation"
+                      defaultValue={booking.relation}
+                      fullWidth
+                    />
+                  </Grid>
+
+                  <Grid item xs={xsmall} sm={small} md={medium}>
+                    <TextInput
+                      source="email"
+                      defaultValue={booking.unit}
+                      fullWidth
+                    />
+                  </Grid>
+                  <Grid item xs={xsmall} sm={small} md={medium}>
+                    <BooleanInput
+                      source="active"
+                      defaultValue={booking.active}
+                      fullWidth
+                    />
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Grid>
             <Button source="Booking" />
-          </Box>
+          </div>
+
           <Toolbar>
             <Box display="flex" justifyContent="space-between" width="100%">
-              <Button
-                label="Continue"
-                primary={false}
-                style={styles.button}
-                onClick={this.continue}
-              />
-           
-           
+              <Button label="Continue" primary={true} onClick={this.continue} />
             </Box>
           </Toolbar>
         </Box>
--- frontend/src/app/components/booking/ScheduleForm.js
+++ frontend/src/app/components/booking/ScheduleForm.js
@@ -1,88 +1,116 @@
-import { Box } from '@material-ui/core';
-import React, { Component } from 'react';
-import { BooleanInput, Button, TextInput, Toolbar } from 'react-admin';
+import { Box, Typography } from "@material-ui/core";
+import React, { Component } from "react";
+import { BooleanInput, Button, TextInput, Toolbar } from "react-admin";
 class ScheduleForm extends Component {
-    continue = e => {
-        e.preventDefault();
-        this.props.nextStep();
+  continue = (e) => {
+    e.preventDefault();
+    this.props.nextStep();
+  };
+  back = (e) => {
+    e.preventDefault();
+    this.props.prevStep();
+  };
 
+  render() {
+    const { schedule, step, handleChange, ScheduleForm } = this.props;
+    const styles = {
+      button: {
+        margin: 15,
+      },
+    };
+
+    console.log(schedule);
+    if (step == 2) {
+      return (
+        <Box p="1em">
+          <Typography variant="h6" gutterBottom>
+            Booking Schedule
+          </Typography>
+          <Box>
+            <TextInput disabled source="id" />
+            <TextInput source="name" defaultValue={schedule.name} />
+            <TextInput
+              source="date"
+              /*options={{ multiLine: true }}*/ defaultValue={schedule.date}
+            />
+            <TextInput multiline source="unit" defaultValue={schedule.unit} />
+            <TextInput source="type" defaultValue={schedule.type} />
+            <TextInput source="floor" defaultValue={schedule.floor} />
+            <TextInput source="block" defaultValue={schedule.block} />
+            <TextInput source="contact" defaultValue={schedule.contact} />
+            <TextInput source="total_cost" defaultValue={schedule.total_cost} />
+            <TextInput source="on_booking" defaultValue={schedule.on_booking} />
+            <TextInput
+              source="on_allocation"
+              defaultValue={schedule.on_allocation}
+            />
+            <TextInput
+              source="on_confirmation"
+              defaultValue={schedule.on_confirmation}
+            />
+            <TextInput source="on_start" defaultValue={schedule.on_start} />
+            <TextInput
+              source="monthly_installment"
+              defaultValue={schedule.monthly_installment}
+            />
+            <TextInput
+              source="quaterly_payment"
+              defaultValue={schedule.quaterly_payment}
+            />
+            <TextInput
+              source="on_excavation"
+              defaultValue={schedule._on_excavation}
+            />
+            <TextInput
+              source="on_foundation"
+              defaultValue={schedule.on_foundation}
+            />
+            <TextInput source="on_slab" defaultValue={schedule.on_slab} />
+            <TextInput source="on_block" defaultValue={schedule.on_block} />
+            <TextInput source="on_plaster" defaultValue={schedule.on_plaster} />
+            <TextInput
+              source="on_plumbing"
+              defaultValue={schedule.on_plumbing}
+            />
+            <TextInput
+              source="on_electric"
+              defaultValue={schedule.on_electric}
+            />
+            <BooleanInput
+              source="on_coloring"
+              defaultValue={schedule.on_coloring}
+            />
+            <BooleanInput
+              source="on_finishing"
+              defaultValue={schedule.on_flooring}
+            />
+            <BooleanInput
+              source="on_possesion"
+              defaultValue={schedule.on_possesion}
+            />
+          </Box>
+          <Toolbar>
+            <Box display="flex" justifyContent="space-between" width="100%">
+              <Button
+                label="Back"
+                primary={false}
+                style={styles.button}
+                onClick={this.back}
+              />
+
+              <Button
+                label="Continue"
+                primary={false}
+                style={styles.button}
+                onClick={this.continue}
+              />
+            </Box>
+          </Toolbar>
+        </Box>
+      );
     }
-    back = e => {
-        e.preventDefault();   
-        this.props.prevStep();
-    
-    }
-
-    render() {
-        const { schedule, step, handleChange, ScheduleForm } = this.props;
-        const styles = {
-            button: {
-                margin: 15
-            }
-        }
-
-        console.log(schedule)
-        if (step == 2) {
-            return (
-                <Box p="1em">
-                    <Box>
-
-                        <TextInput disabled source="id" />
-                        <TextInput source="name" defaultValue={schedule.name}/>
-                        <TextInput source="date" /*options={{ multiLine: true }}*/ defaultValue={schedule.date}/>
-                        <TextInput multiline source="unit"defaultValue={schedule.unit} />
-                        <TextInput source="type" defaultValue={schedule.type}/>
-                        <TextInput source="floor" defaultValue={schedule.floor}/>
-                        <TextInput source="block"defaultValue={schedule.block} />
-                        <TextInput source="contact"defaultValue={schedule.contact} />
-                        <TextInput source="total_cost"defaultValue={schedule.total_cost} />
-                        <TextInput source="on_booking" defaultValue={schedule.on_booking}/>
-                        <TextInput source="on_allocation" defaultValue={schedule.on_allocation}/>
-                        <TextInput source="on_confirmation"defaultValue={schedule.on_confirmation} />
-                        <TextInput source="on_start"defaultValue={schedule.on_start} />
-                        <TextInput source="monthly_installment" defaultValue={schedule.monthly_installment}/>
-                        <TextInput source="quaterly_payment"defaultValue={schedule.quaterly_payment} />
-                        <TextInput source="on_excavation" defaultValue={schedule._on_excavation}/>
-                        <TextInput source="on_foundation"defaultValue={schedule.on_foundation} />
-                        <TextInput source="on_slab"defaultValue={schedule.on_slab} />
-                        <TextInput source="on_block"defaultValue={schedule.on_block} />
-                        <TextInput source="on_plaster" defaultValue={schedule.on_plaster}/>
-                        <TextInput source="on_plumbing" defaultValue={schedule.on_plumbing}/>
-                        <TextInput source="on_electric"defaultValue={schedule.on_electric} />
-                        <BooleanInput source="on_coloring"defaultValue={schedule.on_coloring}/>
-                        <BooleanInput source="on_finishing"defaultValue={schedule.on_flooring} />
-                        <BooleanInput source="on_possesion"defaultValue={schedule.on_possesion} />
-
-
-                    </Box><Toolbar>
-                        <Box display="flex" justifyContent="space-between" width="100%">
-                            <Button
-                                label="Back"
-                                primary={false}
-                                style={styles.button}
-                                onClick={this.back}
-                            />
-
-                            <Button
-
-                                label="Continue"
-                                primary={false}
-                                style={styles.button}
-                                onClick={this.continue}
-
-
-                            />
-
-                        </Box>
-                    </Toolbar>
-                </Box>
-            )
-        }
-        return null;
-    }
+    return null;
+  }
 }
-
-
-
 
 export default ScheduleForm;
