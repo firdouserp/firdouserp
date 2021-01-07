c298755596aa5827097bb876f8fc441b88865390
azekool <aziz.articles@gmail.com>
2021-01-07 02:43:52 +0100

    auth error

--- frontend/src/app/components/Dashboard.js
+++ frontend/src/app/components/Dashboard.js
@@ -1,3 +1,4 @@
+import { Grid } from '@material-ui/core';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import * as React from "react";
@@ -9,6 +10,15 @@ import VouchersChart from './charts/VouchersChart';
 export default () => (
     <Card style={{ height: '100%' }}>
         <Title title="Welcome to the Firdous ERP" />
-        <CardContent><DummyChart /><VouchersChart /></CardContent>
+        <CardContent>
+            <Grid container spacing={6}>
+                <Grid item xs={12} md={6}>
+                    <DummyChart />
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <VouchersChart />
+                </Grid>
+            </Grid>
+        </CardContent>
     </Card>
 );
