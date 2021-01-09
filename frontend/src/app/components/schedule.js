3293d1ec7e2dac51e2103bd940ab9ff04dcb9ea3
azekool <aziz.articles@gmail.com>
2021-01-09 16:11:06 +0100

    scheduleform

--- frontend/src/app/components/schedule.js
+++ frontend/src/app/components/schedule.js
@@ -3,6 +3,7 @@ import ChevronLeft from '@material-ui/icons/ChevronLeft';
 import StoreIcon from '@material-ui/icons/Store';
 import * as React from "react";
 import { Button, Create, Datagrid, DateInput, DeleteButton, Edit, EditButton, Filter, List, ListButton, NumberInput, SearchInput, SimpleForm, SimpleList, TextField, TextInput, TopToolbar } from 'react-admin';
+
 const small = 6;
 const xsmall = 12;
 const medium = 6;
@@ -136,7 +137,7 @@ export const ScheduleEdit = (props) => (
 );
 
 export const ScheduleCreate = (props) => (
-  
+
   <Create undoable={false} title="New Schedule" {...props}>
     <SimpleForm
       variant={"standard"}
@@ -144,184 +145,185 @@ export const ScheduleCreate = (props) => (
       margin="none"
       fullWidth
     >
-     <Box p="1em" width="100%">
+      <di>
+        <Box p="1em" width="100%">
           <Typography variant="h6" gutterBottom>
             Payment Schedule
           </Typography>
-            <Grid container display="flex" spacing={7}>
-              <Grid spacing={1} id="left-container" item xs={xsmall} md={4}>
-                <Typography variant="h7" gutterBottom>
-                  Details
+          <Grid container fullWidth display="flex" spacing={7}>
+            <Grid spacing={1} id="left-container" item xs={xsmall} md={4}>
+              <Typography variant="h7" gutterBottom>
+                Details
                 </Typography>
-                <Grid container spacing={1}>
-                  <Grid item xs={xsmall} sm={small} md={medium}>
-                    <TextInput disabled source="id" fullWidth  />
-                  </Grid>
-                  <Grid item xs={xsmall} sm={small} md={medium}>
-                    <TextInput
-                      source="name"
-                      
-                      fullWidth 
-                    />
-                  </Grid>
-                  <Grid item xs={xsmall} sm={small} md={medium}>
-                    <TextInput
-                      source="unit"
-                      
-                      fullWidth 
-                    />
-                  </Grid>
-                  <Grid item xs={xsmall} sm={small} md={medium}>
-                    <TextInput
-                      source="type"
-                     
-                      fullWidth 
-                    />
-                  </Grid>
-                  <Grid item xs={xsmall} sm={small} md={medium}>
-                    <TextInput
-                      source="block"
-                      
-                      fullWidth  /*options={{ multiLine: true }}*/
-                    />
-                  </Grid>
-                  <Grid item xs={xsmall} sm={small} md={medium}>
-                    <TextInput fullWidth  multiline source="contact" />
-                  </Grid>
-                  <Grid item xs={xsmall} sm={small} md={medium}>
-                    <TextInput
-                      fullWidth 
-                      source="total_cost"
-                      
-                    />
-                  </Grid>
-                  <Grid item xs={xsmall} sm={small} md={medium} lg={medium}>
-                    <DateInput
-                      source="on_booking"
-                    
-                      fullWidth 
-                    />
-                  </Grid>
-                  <Grid item xs={xsmall} sm={small} md={medium}>
-                    <TextInput
-                      source="on_allocation"
-                     
-                      fullWidth 
-                    />
-                  </Grid>
-                  <Grid item xs={xsmall} sm={small} md={medium}>
-                    <TextInput
-                      source="on_confirmation"
-                     
-                      fullWidth 
-                      multiline
-                    />
-                  </Grid>
+              <Grid container spacing={1}>
+                <Grid item xs={xsmall} sm={small} md={medium}>
+                  <TextInput disabled source="id" fullWidth />
                 </Grid>
-              </Grid>
+                <Grid item xs={xsmall} sm={small} md={medium}>
+                  <TextInput
+                    source="name"
 
-              <Grid spacing={1} id="right-container" item xs={xsmall} md={4}>
-                <Typography variant="h7" gutterBottom>
-                  Payment Details
-                </Typography>
-                <Grid container spacing={2}>
-                  <Grid item xs={xsmall} sm={small} md={medium} lg={medium}>
-                    <TextInput
-                      source="on_start"
-                      
-                      fullWidth 
-                    />
-                  </Grid>
-                  <Grid item xs={xsmall} sm={small} md={medium}>
-                    <TextInput
-                      source="monthly_installment"
-                     
-                      fullWidth 
-                    />
-                  </Grid>
-                  <Grid item xs={xsmall} sm={small} md={medium}>
-                    <TextInput
-                      source="quaterly_payment"
-                     
-                      fullWidth
-                    />
-                  </Grid>
-                  <Grid item xs={xsmall} sm={small} md={medium}>
-                    <TextInput
-                      source="on_excavation"
-                      
-                      fullWidth 
-                    />
-                  </Grid>
-                  <Grid item xs={xsmall} sm={small} md={medium}>
-                    <TextInput
-                      source="on_foundation"
-                    
-                      fullWidth 
-                    />
-                  </Grid>
-                  <Grid item xs={xsmall} sm={small} md={medium}>
-                    <TextInput
-                      source="on_slab"
-                      
-                      fullWidth 
-                    />
-                  </Grid>
-                  <Grid item xs={xsmall} sm={small} md={medium}>
-                    <TextInput
-                      source="on_block"
-                     
-                      fullWidth 
-                    />
-                  </Grid>
-                  <Grid item xs={xsmall} sm={small} md={medium}>
-                    <TextInput
-                      source="on_plaster"
-                  
-                      fullWidth 
-                    />
-                  </Grid>
-                  <Grid item xs={xsmall} sm={small} md={medium}>
-                    <TextInput
-                      source="on_plumbing"
-                      fullWidth 
-                    />
-                  </Grid>
-                  <Grid item xs={xsmall} sm={small} md={medium}>
-                    <TextInput
-                      source="on_electric"
-                      fullWidth 
-                    />
-                  </Grid>
-                  <Grid item xs={xsmall} sm={small} md={medium}>
-                    <TextInput
-                      source="on_coloring"
-                      
-                      fullWidth 
-                    />
-                  </Grid>
-                  <Grid item xs={xsmall} sm={small} md={medium}>
-                    <TextInput
-                      source="on_finishing"
-                      
-                      fullWidth 
-                    />
-                  </Grid>
-                  <Grid item xs={xsmall} sm={small} md={medium}>
-                    <TextInput
-                      source="on_possesion"
-                      
-                      fullWidth 
-                    />
-                  </Grid>
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item xs={xsmall} sm={small} md={medium}>
+                  <TextInput
+                    source="unit"
+
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item xs={xsmall} sm={small} md={medium}>
+                  <TextInput
+                    source="type"
+
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item xs={xsmall} sm={small} md={medium}>
+                  <TextInput
+                    source="block"
+
+                    fullWidth  /*options={{ multiLine: true }}*/
+                  />
+                </Grid>
+                <Grid item xs={xsmall} sm={small} md={medium}>
+                  <TextInput fullWidth multiline source="contact" />
+                </Grid>
+                <Grid item xs={xsmall} sm={small} md={medium}>
+                  <TextInput
+                    fullWidth
+                    source="total_cost"
+
+                  />
+                </Grid>
+                <Grid item xs={xsmall} sm={small} md={medium} lg={medium}>
+                  <DateInput
+                    source="on_booking"
+
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item xs={xsmall} sm={small} md={medium}>
+                  <TextInput
+                    source="on_allocation"
+
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item xs={xsmall} sm={small} md={medium}>
+                  <TextInput
+                    source="on_confirmation"
+
+                    fullWidth
+                    multiline
+                  />
                 </Grid>
               </Grid>
             </Grid>
-            <Button source="Schedule" />
-          
 
-          </Box>
-        
+            <Grid spacing={1} id="right-container" item xs={xsmall} md={4}>
+              <Typography variant="h7" gutterBottom>
+                Payment Details
+                </Typography>
+              <Grid container spacing={2}>
+                <Grid item xs={xsmall} sm={small} md={medium} lg={medium}>
+                  <TextInput
+                    source="on_start"
+
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item xs={xsmall} sm={small} md={medium}>
+                  <TextInput
+                    source="monthly_installment"
+
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item xs={xsmall} sm={small} md={medium}>
+                  <TextInput
+                    source="quaterly_payment"
+
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item xs={xsmall} sm={small} md={medium}>
+                  <TextInput
+                    source="on_excavation"
+
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item xs={xsmall} sm={small} md={medium}>
+                  <TextInput
+                    source="on_foundation"
+
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item xs={xsmall} sm={small} md={medium}>
+                  <TextInput
+                    source="on_slab"
+
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item xs={xsmall} sm={small} md={medium}>
+                  <TextInput
+                    source="on_block"
+
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item xs={xsmall} sm={small} md={medium}>
+                  <TextInput
+                    source="on_plaster"
+
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item xs={xsmall} sm={small} md={medium}>
+                  <TextInput
+                    source="on_plumbing"
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item xs={xsmall} sm={small} md={medium}>
+                  <TextInput
+                    source="on_electric"
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item xs={xsmall} sm={small} md={medium}>
+                  <TextInput
+                    source="on_coloring"
+
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item xs={xsmall} sm={small} md={medium}>
+                  <TextInput
+                    source="on_finishing"
+
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item xs={xsmall} sm={small} md={medium}>
+                  <TextInput
+                    source="on_possesion"
+
+                    fullWidth
+                  />
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Button source="Schedule" />
+
+
+        </Box>
+      </di>
     </SimpleForm>
   </Create>
 );
