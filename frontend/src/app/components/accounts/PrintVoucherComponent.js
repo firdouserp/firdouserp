b8f60e17ea79a3a14437907534a0dc833b49a51f
aziz.rehman <aziz.rehman@alfa.de>
2021-01-14 15:15:09 +0100

    voucher

--- frontend/src/app/components/accounts/PrintVoucherComponent.js
+++ frontend/src/app/components/accounts/PrintVoucherComponent.js
@@ -1,29 +1,28 @@
-import React, { Component } from 'react';
-import Voucher from './VoucherPrint';
+import React, { Component } from "react";
+import logo from "../../assets/firdouslogo.png";
+import Voucher from "./VoucherPrint";
 export class PrintVoucherComponent extends Component {
-
-    render() {
-        const { record, loaded } = this.props;
-        if (record.id) {
-            return (
-                <div>
-
-                    <Voucher
-
-                        voucher={{ ...record }}
-                        // invoice={{ "createdDate": "25-5-2021", "dueDate": "23-2-9892", "paidDate": "25,1,2021", "paymentMethod": "CASH", "id": "1", "description": "this is description", "items": [{ 'description': 'item1', 'amount': '25' }], "name": "Firdous REsidecy" }}
-                        //customer={{}}
-                        company={{ "name": "Firdous ERP", "logoUrl": "https://dummyimage.com/600x400/000/fff" }}
-                        notes={"Here comes the Remarks"}
-                    />
-
-                </div>
-
-
-            )
-        }
-        return null;
+  render() {
+    const { record, loaded } = this.props;
+    if (record.id) {
+      console.log(logo);
+      return (
+        <div>
+          <Voucher
+            voucher={{ ...record }}
+            // invoice={{ "createdDate": "25-5-2021", "dueDate": "23-2-9892", "paidDate": "25,1,2021", "paymentMethod": "CASH", "id": "1", "description": "this is description", "items": [{ 'description': 'item1', 'amount': '25' }], "name": "Firdous REsidecy" }}
+            //customer={{}}
+            company={{
+              name: "Firdous ERP",
+              logoUrl: logo,
+            }}
+            notes={"Here comes the Remarks"}
+          />
+        </div>
+      );
     }
+    return null;
+  }
 }
 
-export default PrintVoucherComponent
+export default PrintVoucherComponent;
