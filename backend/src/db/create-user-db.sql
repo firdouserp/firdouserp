ec998ddf5c7e639aa90de780f2375f7e89a94b60
azekool <aziz.articles@gmail.com>
2021-01-21 00:56:09 +0100

    view project

--- backend/src/db/create-user-db.sql
+++ backend/src/db/create-user-db.sql
@@ -187,3 +187,37 @@ CREATE TABLE `grn_details` (
 );
 ALTER TABLE `firdouserp`.`purchase_order` 
 CHANGE COLUMN `status` `status` INT NULL DEFAULT NULL ;
+
+CREATE VIEW view_project_ledger
+AS
+SELECT 
+       DATE (Vou_Date) AS Vou_Date, 
+       Vou_No,ledger.Description|| " " || Chq_No AS Description, 
+       DR, 
+       CR, 
+       Chq_No, 
+       Chq_Date, 
+       RefNo, 
+       Project, 
+       projects.Code AS Project_Code, 
+       Projects.Title AS Project_Title, 
+       Supplier, 
+       Suppliers.Code AS Supplier_Code, 
+       Suppliers.Title AS Supplier_Title, 
+       COA, 
+       COA.Code AS COA_Code, 
+       COA.TITLE AS COA_TITLE, 
+       Unit, 
+       Units.Code AS Unit_Code, 
+       Units.Title AS Unit_Title
+FROM   Ledger
+       LEFT JOIN COA ON Ledger.COA = COA.ID
+       LEFT JOIN Suppliers ON Ledger.Supplier= Suppliers.ID
+       LEFT JOIN Projects ON Ledger.Project = Projects.ID
+       LEFT JOIN Units ON Ledger.Unit = Units.ID
+ORDER  BY
+          Vou_Date,
+          Project,
+          Supplier,
+          COA,
+          Unit
